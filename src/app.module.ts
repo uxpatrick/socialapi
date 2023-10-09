a6cbd94ee0d35c307b9cce76fcf033a0eb17bfac
bukej <yaaqov147@gmail.com>
2023-10-09 18:22:48 +0200

    users endpoint creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
+import { UsersModule } from '../users/users.module';
+import { UsersController } from '../users/users.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    AppModule,
+    UsersModule
+        ],
+  controllers: [
+        AppController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ users/users.controller.ts
@@ -0,0 +1,16 @@
+/*
+https://docs.nestjs.com/controllers#controllers
+*/
+
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Get()
+    getUsers(): string {
+      return this.usersService.getUsers();
+    }
+}
